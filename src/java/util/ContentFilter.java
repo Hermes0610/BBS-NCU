f215712917b9e73de7be2901adf87a93be32a899
z1412192088 <1412192088@qq.com>
2020-12-21 23:46:48 +0800

    修改代码

--- src/java/util/ContentFilter.java
+++ src/java/util/ContentFilter.java
@@ -2,28 +2,18 @@ package util;
 
 import util.threadlocal.LocalRequestContextHolder;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * 过滤器，将当前保存当前request response session 进入到当前线程当中
- */
 public class ContentFilter extends org.springframework.web.filter.CharacterEncodingFilter {
-    private FilterConfig filterConfig;
-
-    // Handle the passed-in FilterConfig
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //request.setCharacterEncoding("gb2312");
-        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletRequest req = request;
         LocalRequestContextHolder.setLocalRequestContext(req,response );
-        //filterChain.doFilter(request, response);
 
         super.doFilterInternal(request, response, filterChain);
     }
